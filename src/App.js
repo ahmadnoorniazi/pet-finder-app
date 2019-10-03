7e175580382aa0deaa019ff57208c69b4f396195
Ahmad Noor <ahmad.noor@mobilelive.ca>
2019-10-03 17:49:14 +0500

    Update .babelrc

--- src/App.js
+++ src/App.js
@@ -3,44 +3,49 @@ import { render } from "react-dom";
 import pf from "petfinder-client";
 import { Provider } from "./SearchContext";
 import Main from "./Main";
+
 const petfinder = pf({
   key: process.env.API_KEY,
   secret: process.env.API_SECRET
 });
-export default function App(){
-  const [location, setLocation] = useState("Seattle, WA")
-  const [animal, setAnimal] = useState("")
-  const [breed, setBreed] = useState("")
-  const [breeds, setBreeds] = useState([])
 
-  function handleLocationChange(event){
-    setLocation(event.target.value)
-  };
+export default function App() {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
-  function handleAnimalChange(event){
-    setAnimal(event.target.value)
-    setBreed("")
-    getBreeds()
-  };
-
-  function handleBreedChange(event){
-    setBreed(event.target.value)
+  function handleLocationChange(event) {
+    setLocation(event.target.value);
   }
-  function getBreeds(){
+
+  function handleAnimalChange(event) {
+    setAnimal(event.target.value);
+    setBreed("");
+    getBreeds();
+  }
+
+  function handleBreedChange(event) {
+    setBreed(event.target.value);
+  }
+
+  function getBreeds() {
     if (animal) {
-      petfinder.breed.list({
-        animal: animal
-      }).then(data => {
-        if (
-          data.petfinder &&
-          data.petfinder.breeds &&
-          Array.isArray(data.petfinder.breeds.breed)
-        ) {
-          setBreeds(data.petfinder.breeds.breed);
-        } else {
-          setBreeds([])
-        }
-      });
+      petfinder.breed
+        .list({
+          animal: animal
+        })
+        .then(data => {
+          if (
+            data.petfinder &&
+            data.petfinder.breeds &&
+            Array.isArray(data.petfinder.breeds.breed)
+          ) {
+            setBreeds(data.petfinder.breeds.breed);
+          } else {
+            setBreeds([]);
+          }
+        });
     }
   }
   const searchContext = {
@@ -52,11 +57,11 @@ export default function App(){
     handleBreedChange,
     handleLocationChange,
     getBreeds
-  }
-    return (
-      <Provider value={searchContext}>
-       <Main />
-      </Provider>
-    )
-    }
-render(React.createElement(App), document.getElementById("root"))
+  };
+  return (
+    <Provider value={searchContext}>
+      <Main />
+    </Provider>
+  );
+}
+render(React.createElement(App), document.getElementById("root"));

140915cd358c2b7ee56519b185a2e9a83cbedd38
Ahmad Noor <ahmad.noor@mobilelive.ca>
2019-10-05 19:12:01 +0500

    Update Pet Component

--- src/Pet.js
+++ src/Pet.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { Link } from "@reach/router";
 
-function  Pet({name, animal, breed, media, location, id}) {
-    let photos = [];
+const Pet = props => {
+  const { name, animal, breed, media, location, id } = props;
 
-    if (media && media.photos && media.photos.photo) {
-      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
-    }
-    const hero = photos[0] ? photos[0].value : "http://placecorgi.com/300/300";
-    return (
-      <Link to={`/details/${id}`} className="pet">
-        <div className="image-container">
-          <img src={hero} alt={name} />
-        </div>
-        <div className="info">
-          <h1>{name}</h1>
-          <h2>
-            {animal} - {breed} - {location}
-          </h2>
-        </div>
-      </Link>
-    );
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
   }
+
+  return (
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </Link>
+  );
+};
 
 export default Pet;

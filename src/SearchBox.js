d214228e207c485075c113d2099c2e084b694fe8
Ahmad Noor <ahmad.noor@mobilelive.ca>
2019-10-05 19:10:23 +0500

    Update Search Box  component

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,62 +1,73 @@
-import React, { useContext } from "react";
-import { ANIMALS } from "petfinder-client";
-import { SearchContext } from "./SearchContext";
+import React, { useState, useEffect, useContext } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropDown";
+import Results from "./Results";
+import ThemeContext from "./ThemeContext";
 
-function SearchBox(props){
-  function handleSearch(event){
-    event.preventDefault();
-    props.search();
-  };
-    const context = useContext(SearchContext)
-    return (
-          <div className="search-params">
-            <form onSubmit={handleSearch}>
-              <label htmlFor="location">
-                Location
-                <input
-                  id="location"
-                  value={context.location}
-                  placeholder="Location"
-                  onChange={context.handleLocationChange}
-                />
-              </label>
-              <label htmlFor="animal">
-                Animal
-                <select
-                  id="animal"
-                  value={context.animal}
-                  onChange={context.handleAnimalChange}
-                  onBlur={context.handleAnimalChange}
-                >
-                  <option />
-                  {ANIMALS.map(animal => (
-                    <option key={animal} value={animal}>
-                      {animal}
-                    </option>
-                  ))}
-                </select>
-              </label>
-              <label htmlFor="breed">
-                Breed
-                <select
-                  id="breed"
-                  value={context.breed}
-                  onChange={context.handleBreedChange}
-                  onBlur={context.handleBreedChange}
-                  disabled={!context.breeds.length}
-                >
-                  <option />
-                  {context.breeds.map(breed => (
-                    <option key={breed} value={breed}>
-                      {breed}
-                    </option>
-                  ))}
-                </select>
-              </label>
-              <button>Submit</button>
-            </form>
-          </div>
-    );
+const SearchParams = () => {
+  const [theme, setTheme] = useContext(ThemeContext);
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([]);
+  const [pets, setPets] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+    setPets(animals || []);
   }
 
-export default SearchBox;
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+    }, console.error);
+  }, [animal]);
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <label htmlFor="location">
+          Theme
+          <select
+            value={theme}
+            onChange={e => setTheme(e.target.value)}
+            onBlur={e => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;

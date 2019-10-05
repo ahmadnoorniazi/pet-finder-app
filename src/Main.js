f9420bff0874cef8aad70d6dae062826eb376803
Ahmad Noor <ahmad.noor@mobilelive.ca>
2019-10-05 19:12:59 +0500

    Update Main Component

--- src/Main.js
+++ src/Main.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import {
-    Router,
-    Link
-  } from "@reach/router";
-  import Details from "./Details";
-  import SearchParams from "./SearchParams";
-  import Results from "./Results";
+import React from "react";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import Results from "./Results";
 
 const Main = () => {
-    return (
-        <div>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-            <Link to="/search-params">
-              <span aria-label="search" role="img">
-                🔍
-              </span>
-            </Link>
-          </header>
-          <Router>
-            <Results path="/" />
-            <Details path="/details/:id" />
-            <SearchParams path="/search-params" />
-          </Router>
-        </div>
-    );
-}
+  return (
+    <div>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+        <Link to="/search-params">
+          <span aria-label="search" role="img">
+            🔍
+          </span>
+        </Link>
+      </header>
+      <Router>
+        <Results path="/" />
+        <Details path="/details/:id" />
+        <SearchParams path="/search-params" />
+      </Router>
+    </div>
+  );
+};
 
 export default Main;

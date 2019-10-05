979ce707b469eeed52ce041b93a431eb7a9b92ab
Ahmad Noor <ahmad.noor@mobilelive.ca>
2019-10-05 19:11:24 +0500

    Update search Params Component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,15 @@
 import React from "react";
 import SearchBox from "./SearchBox";
 import { navigate } from "@reach/router";
-function Search(){
-
+function Search() {
   function search() {
     navigate("/");
   }
-    return (
-      <div className="search-route">
-        <SearchBox search={search} />
-      </div>
-    );
-  }
+  return (
+    <div className="search-route">
+      <SearchBox search={search} />
+    </div>
+  );
+}
 
 export default Search;
